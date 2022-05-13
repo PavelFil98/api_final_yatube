3c9b7b3f5e2c357a2b9aada91d9e5fd414bd54f8
Pavel Filipovich <pavelfilipovich@MacBook-Pro-Pavel.local>
2022-05-13 14:08:48 +0300

    1

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from posts.models import Post, Group, Comment, Follow
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'text',
+        'pub_date',
+        'author',
+        'group'
+    )
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'title',
+        'slug'
+    )
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'post',
+        'author',
+        'text',
+        'created',
+    )
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'following'
+    )
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Follow, FollowAdmin)
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,23 +4,101 @@ from django.db import models
 User = get_user_model()
 
 
-class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
-    image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
+class Group(models.Model):
+    title = models.CharField(verbose_name='Title', max_length=200)
+    slug = models.SlugField(verbose_name='slug', unique=True)
+    description = models.TextField(verbose_name='description')
 
     def __str__(self):
-        return self.text
+        return {self.title}
+
+
+class Post(models.Model):
+    text = models.TextField(
+        verbose_name='text',
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='publication date',
+        auto_now_add=True
+    )
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        related_name='posts',
+        verbose_name='Группа',
+        blank=True, null=True,
+        help_text='Выберите группу'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        related_name='posts',
+        verbose_name='author',
+        null=True,
+    )
+    image = models.ImageField(
+        verbose_name='image',
+        upload_to='posts/',
+        blank=True
+    )
+
+    class Meta:
+        ordering = ('pub_date',)
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
+
+    def __str__(self):
+        return self.text[:15]
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
+        Post,
+        on_delete=models.SET_NULL,
+        related_name='comments',
+        verbose_name='post',
+        blank=True, null=True,
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        related_name='comments',
+        verbose_name='author',
+        null=True,
+    )
+    text = models.TextField(
+        verbose_name='text',
+    )
     created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+        verbose_name='create date',
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.text[:20]
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        related_name='follower',
+        verbose_name='user',
+        null=True,
+    )
+    following = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        related_name='following',
+        verbose_name='follow_user',
+        null=True,
+    )
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('following', 'user'), name='unique_following'),
+        )
+
+    def __str__(self):
+        return f'{self.user} подписан на {self.following}'
